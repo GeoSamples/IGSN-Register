4d3c386a7b499f71340070b1206a1ace1340421a
Jeff Heard <jefferson.r.heard@gmail.com>
2014-02-20 15:31:47 -0500

    Removed tastypie dependency

--- admin.py
+++ admin.py
@@ -1,6 +1,4 @@
 from django.contrib.gis import admin
-from tastypie.admin import ApiKeyInline
-from tastypie.models import ApiAccess, ApiKey
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from sesar_mobile.models import UserProfile
