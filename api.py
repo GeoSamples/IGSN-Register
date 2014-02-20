97be247588a04bf202c776555279cf52e2e9ef3b
Jeff Heard <jefferson.r.heard@gmail.com>
2014-02-20 15:32:22 -0500

    Removed Tastypie API from the repo

--- api.py
+++
@@ -1,5 +0,0 @@
-from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS	
-from tastypie.api import Api
-from tastypie.authentication import BasicAuthentication
-from tastypie.authorization import Authorization
-from ga_ows.tastyhacks import GeoResource
